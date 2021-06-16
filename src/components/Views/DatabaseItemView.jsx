320d02ef914f86c3bf2ee6401aaaec652d68d84a
kreafox <krisztina.n.elekes@gmail.com>
2021-06-16 13:31:13 +0300

    Add webmap for DatabaseItemView; Fix footer

--- src/components/Views/DatabaseItemView.jsx
+++ src/components/Views/DatabaseItemView.jsx
@@ -5,12 +5,14 @@ import { DefaultView } from '@plone/volto/components';
 const DatabaseItemView = (props) => {
   const { content } = props;
   const tableau_url = content?.embed_url;
+  const map_url = content?.webmap_url;
   const lead_image = content?.image;
   const {
     blocks: { blocksConfig },
   } = config;
 
   const TableauBlockView = blocksConfig.tableau_block.view;
+  const MapBlockView = blocksConfig.maps.view;
 
   return (
     <>
@@ -20,6 +22,12 @@ const DatabaseItemView = (props) => {
         {tableau_url && (
           <div style={{ margin: '1em 0' }}>
             <TableauBlockView {...props} data={{ url: tableau_url }} />
+          </div>
+        )}
+
+        {map_url && (
+          <div style={{ margin: '1em 0' }}>
+            <MapBlockView {...props} data={{ url: map_url }} />
           </div>
         )}
       </div>
