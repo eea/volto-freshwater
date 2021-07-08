d52c09bf14113060b20f9bd497dd0e2341cf248d
kreafox <krisztina.n.elekes@gmail.com>
2021-07-08 13:03:27 +0300

    Add item type for simple listing & Styling

--- src/components/theme/SimpleListing/SimpleListingView.jsx
+++ src/components/theme/SimpleListing/SimpleListingView.jsx
@@ -6,6 +6,16 @@ import ItemMetadataSnippet from './../ItemMetadata/ItemMetadataSnippet';
 import ItemTitle from './../ItemMetadata/ItemTitle';
 import './style.less';
 
+const formatItemType = (item) => {
+  const type =
+    item
+      .replace('_', ' / ')
+      .split(' ')
+      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+      .join(' ') || '';
+  return type;
+};
+
 const SimpleListingView = ({ items, isEditMode }) => {
   const [isOpenModal, setOpenModal] = React.useState(false);
   const [selectedItem, setSelectedItem] = React.useState(null);
@@ -20,21 +30,24 @@ const SimpleListingView = ({ items, isEditMode }) => {
       <ul className="items">
         {items.map((item) => (
           <li className="simple-listing-item" key={item['@id']}>
-            <div className="listing-body">
-              <div
-                className="listing-title"
-                onClick={() => {
-                  setOpenModal(true);
-                  setSelectedItem(item);
-                }}
-                onKeyDown={() => setSelectedItem(item)}
-                role="button"
-                tabIndex="0"
-              >
-                <div className="simple-listing-title">
-                  {item.title ? item.title : item.id}
-                </div>
+            <div
+              className="simple-listing"
+              onClick={() => {
+                setOpenModal(true);
+                setSelectedItem(item);
+              }}
+              onKeyDown={() => setSelectedItem(item)}
+              role="button"
+              tabIndex="0"
+            >
+              <div className="simple-listing-title">
+                {item.title ? item.title : item.id}
               </div>
+              {item['@type'] && (
+                <div className="metadata-tab-section">
+                  <span>{formatItemType(item['@type'])}</span>
+                </div>
+              )}
             </div>
           </li>
         ))}
