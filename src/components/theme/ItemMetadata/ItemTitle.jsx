ec44dd85e25d4b805da691a6c5355f9aeb7a3fe3
kreafox <krisztina.n.elekes@gmail.com>
2021-08-04 17:23:28 +0300

    Add title link for metadata modal

--- src/components/theme/ItemMetadata/ItemTitle.jsx
+++ src/components/theme/ItemMetadata/ItemTitle.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { injectIntl } from 'react-intl';
 
 const ItemTitle = (props) => {
-  const { item } = props;
+  const { item, token } = props;
+  const url = item.source ? item.source[0]['@id'] : item['@id'];
 
-  return <>{item && <h3>{item.title}</h3>}</>;
+  return (
+    <>
+      {token ? (
+        <>
+          {item && (
+            <h3>
+              <a href={url}>{item.title}</a>
+            </h3>
+          )}
+        </>
+      ) : (
+        <>{item && <h3>{item.title}</h3>}</>
+      )}
+    </>
+  );
 };
 
-export default ItemTitle;
+export default compose(
+  withRouter,
+  injectIntl,
+)(
+  connect((state) => ({
+    token: state.userSession.token,
+  }))(ItemTitle),
+);
