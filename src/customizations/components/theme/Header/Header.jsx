e6428fd4500683ce8804441ad7deaa712be255aa
kreafox <krisztina.n.elekes@gmail.com>
2021-11-12 16:04:29 +0200

    Prettier

--- src/customizations/components/theme/Header/Header.jsx
+++ src/customizations/components/theme/Header/Header.jsx
@@ -63,7 +63,7 @@ class Header extends Component {
   componentDidUpdate(prevProps) {
     if (prevProps.actualPathName !== this.props.actualPathName) {
       this.setState({
-        isHomepage: this.props.content?.['@type'] === 'Plone Site';
+        isHomepage: this.props.content?.['@type'] === 'Plone Site',
       });
     }
   }
