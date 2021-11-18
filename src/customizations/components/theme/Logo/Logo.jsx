c0cacc1567d7a7f7702ff0427ea74ffe1a23c0d4
kreafox <krisztina.n.elekes@gmail.com>
2021-11-18 13:06:43 +0200

    Remove default logo messages

--- src/customizations/components/theme/Logo/Logo.jsx
+++ src/customizations/components/theme/Logo/Logo.jsx
@@ -5,22 +5,10 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { defineMessages, useIntl } from 'react-intl';
 import { Image } from 'semantic-ui-react';
 import { useSelector } from 'react-redux';
 
-import LogoImage from '@plone/volto/components/theme/Logo/Logo.svg';
-
-const messages = defineMessages({
-  site: {
-    id: 'Site',
-    defaultMessage: 'Site',
-  },
-  plonesite: {
-    id: 'Plone Site',
-    defaultMessage: 'Plone Site',
-  },
-});
+import LogoImage from './Logo.svg';
 
 /**
  * Logo component class.
@@ -29,15 +17,14 @@ const messages = defineMessages({
  * @returns {string} Markup of the component.
  */
 const Logo = (props) => {
-  const intl = useIntl();
   const root = useSelector((state) => state.breadcrumbs.root);
 
   return (
-    <Link to={root || '/'} title={intl.formatMessage(messages.site)}>
+    <Link to={root || '/'} title="WISE Freshwater">
       <Image
         src={LogoImage}
-        alt={intl.formatMessage(messages.plonesite)}
-        title={intl.formatMessage(messages.plonesite)}
+        alt="WISE Freshwater Logo"
+        title="WISE Freshwater"
       />
     </Link>
   );
