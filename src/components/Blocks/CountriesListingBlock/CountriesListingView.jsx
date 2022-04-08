23fae7bbf9f16958417246fc21dfeb6a8618368d
kreafox <krisztina.n.elekes@gmail.com>
2022-04-08 11:28:36 +0300

    Separate european union in countries listing block

--- src/components/Blocks/CountriesListingBlock/CountriesListingView.jsx
+++ src/components/Blocks/CountriesListingBlock/CountriesListingView.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Item } from 'semantic-ui-react';
+import { Item, Grid } from 'semantic-ui-react';
 import { flattenToAppURL, getBaseUrl } from '@plone/volto/helpers';
 import { useSelector, useDispatch } from 'react-redux';
 import { searchContent } from '@plone/volto/actions';
@@ -45,32 +45,68 @@ const CountriesListingView = (props) => {
               {section.title && (
                 <h3 className="section-title">{section.title}</h3>
               )}
-              <div
-                className="countries-listing-section"
-                style={{ columns: data.columnsCount || 3 }}
-              >
-                {section?.items
-                  .sort((a, b) => (a.title > b.title ? 1 : -1))
-                  .map((item, i) => (
-                    <div key={i} className="countries-item-wrapper">
-                      {item.lead_image && (
-                        <Item.Image
-                          className="countries-list-flag"
-                          alt={item.title}
-                          src={`${item['@id']
-                            .replace(config.settings.apiPath, '')
-                            .replace(
-                              config.settings.internalApiPath,
-                              '',
-                            )}/@@images/image/thumb`}
-                        />
-                      )}
-                      <Link to={flattenToAppURL(item['@id'])}>
-                        {item.title}
-                      </Link>
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={8}>
+                    <div
+                      className="countries-listing-section"
+                      style={{ columns: data.columnsCount || 3 }}
+                    >
+                      {section?.items
+                        .sort((a, b) => (a.title > b.title ? 1 : -1))
+                        .filter((a) => a.id !== 'european-union')
+                        .map((item, i) => (
+                          <div key={i} className="countries-item-wrapper">
+                            {item.lead_image && (
+                              <Item.Image
+                                className="countries-list-flag"
+                                alt={item.title}
+                                src={`${item['@id']
+                                  .replace(config.settings.apiPath, '')
+                                  .replace(
+                                    config.settings.internalApiPath,
+                                    '',
+                                  )}/@@images/image/thumb`}
+                              />
+                            )}
+                            <Link
+                              to={flattenToAppURL(item['@id'])}
+                              className="countries-listing-link"
+                            >
+                              {item.title}
+                            </Link>
+                          </div>
+                        ))}
                     </div>
-                  ))}
-              </div>
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    {section?.items
+                      .filter((a) => a.id === 'european-union')
+                      .map((item, i) => (
+                        <div key={i} className="countries-item-wrapper single">
+                          {item.lead_image && (
+                            <Item.Image
+                              className="countries-list-flag"
+                              alt={item.title}
+                              src={`${item['@id']
+                                .replace(config.settings.apiPath, '')
+                                .replace(
+                                  config.settings.internalApiPath,
+                                  '',
+                                )}/@@images/image/thumb`}
+                            />
+                          )}
+                          <Link
+                            to={flattenToAppURL(item['@id'])}
+                            className="countries-listing-link"
+                          >
+                            {item.title}
+                          </Link>
+                        </div>
+                      ))}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
             </div>
           );
         })}
