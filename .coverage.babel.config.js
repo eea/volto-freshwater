eb1c1c2b605ad9bf0e58c06a449803a430a5e4c1
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2022-10-17 18:13:57 +0300

    Fix tests

--- .coverage.babel.config.js
+++ .coverage.babel.config.js
@@ -2,7 +2,15 @@ const defaultBabel = require('@plone/volto/babel');
 
 function applyDefault(api) {
   const voltoBabel = defaultBabel(api);
-  voltoBabel.plugins.push('@babel/plugin-transform-modules-commonjs', 'transform-class-properties', 'istanbul');
+  voltoBabel.plugins.push(
+    '@babel/plugin-transform-modules-commonjs',
+    'transform-class-properties',
+    'istanbul',
+  );
+  voltoBabel.plugins.push(
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  );
   return voltoBabel;
 }
 
