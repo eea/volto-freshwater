46467b041ba9087525fc186e3c489a591277ab1a
Mimi <cretu.mihaelaa11@gmail.com>
2023-06-08 18:18:11 +0300

    modified the NWRM measure template page

--- src/components/theme/MeasureView/MeasureView.jsx
+++ src/components/theme/MeasureView/MeasureView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BodyClass } from '@plone/volto/helpers';
 import { ItemMetadataSnippet } from '@eeacms/volto-freshwater/components';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
@@ -6,6 +6,104 @@ import './style.less';
 
 const MeasureView = (props) => {
   const { content } = props;
+
+  // Add a class with the coresponding level from the table
+  const changeLevelsColor = () => {
+    const fieldItems = document.querySelectorAll('.field--name-field-level');
+    fieldItems.forEach((fieldItem) => {
+      const innerText = fieldItem.textContent;
+
+      if (innerText === 'High') {
+        fieldItem.classList.add('high');
+      }
+      if (innerText === 'Medium') {
+        fieldItem.classList.add('medium');
+      }
+      if (innerText === 'Low') {
+        fieldItem.classList.add('low');
+      }
+    });
+  };
+
+  // Compare alphanumeric strings, used for sorting both lexicographically and numerically
+  const compareAlphanumericStrings = (firstInput, secondInput) => {
+    const regex = /(\d+)|(\D+)/g;
+    const firstInputArray = String(firstInput).match(regex);
+    const secondInputArray = String(secondInput).match(regex);
+
+    while (firstInputArray.length > 0 && secondInputArray.length > 0) {
+      const partA = firstInputArray.shift();
+      const partB = secondInputArray.shift();
+
+      if (partA !== partB) {
+        const isNumberA = /^\d+$/.test(partA);
+        const isNumberB = /^\d+$/.test(partB);
+
+        if (isNumberA && isNumberB) {
+          return Number(partA) - Number(partB);
+        } else if (isNumberA) {
+          return -1;
+        } else if (isNumberB) {
+          return 1;
+        } else {
+          return partA.localeCompare(partB);
+        }
+      }
+    }
+
+    return firstInputArray.length - secondInputArray.length;
+  };
+
+  // Rearrange rows from the table based on their level. (The rows with the level 'High' appear first, then 'Medium,' and then 'Low.')
+  const rearrangeRows = () => {
+    const table = document.querySelector(
+      '#paragraph-nwrm_benefits_w_level tbody',
+    );
+    const rows = Array.from(table.rows);
+
+    let previousLevel = null;
+
+    rows.sort((a, b) => {
+      // Sort rows by levels
+      const levelA = a.querySelector('.field--name-field-level').textContent;
+      const levelB = b.querySelector('.field--name-field-level').textContent;
+      const textA = a.querySelector('.field--name-field-nwrm-benefits-2')
+        .textContent;
+      const textB = b.querySelector('.field--name-field-nwrm-benefits-2')
+        .textContent;
+
+      if (levelA === levelB) {
+        // If levels are the same, sort alphabetically by text
+        return compareAlphanumericStrings(textA, textB);
+      } else if (levelA === 'High') {
+        return -1;
+      } else if (levelB === 'High') {
+        return 1;
+      } else if (levelA === 'Medium') {
+        return -1;
+      } else if (levelB === 'Medium') {
+        return 1;
+      } else {
+        return 0; // Default case: 'Low' or any other value
+      }
+    });
+
+    rows.forEach((row) => {
+      table.appendChild(row);
+      const level = row.querySelector('.field--name-field-level').textContent;
+
+      if (level !== previousLevel) {
+        const levelCell = row.querySelector('.field--name-field-level');
+        levelCell.classList.add('show-level');
+        previousLevel = level;
+      }
+    });
+  };
+
+  useEffect(() => {
+    changeLevelsColor();
+    rearrangeRows();
+  }, []);
 
   return (
     <>
@@ -18,48 +116,7 @@ const MeasureView = (props) => {
 
             <ItemMetadataSnippet {...props} item={content} />
 
-            <div class="field--label-inline">
-              <div class="field__label">Code</div>
-              <div class="field__item">{content.measure_code}</div>
-            </div>
-
-            <div class="field--label-inline">
-              <div class="field__label">Sector</div>
-              <div class="field__item">{content.measure_sector}</div>
-            </div>
-
-            {content.other_sector && (
-              <>
-                <br />
-                <div class="field--label-inline">
-                  <div class="field__label">Other sector(s)</div>
-                  <div class="field__item">{content.other_sector}</div>
-                </div>
-              </>
-            )}
-
-            <br />
             <div>
-              <h4>The complete description of the NWRM</h4>
-              <div class="field__item">
-                {content.items.map(
-                  (item) =>
-                    item['@type'] === 'File' && (
-                      <a
-                        href={item['@id'] + '/@@images/file'}
-                        rel="noreferrer"
-                        target="_blank"
-                      >
-                        {item.title}
-                      </a>
-                    ),
-                )}
-              </div>
-            </div>
-
-            <br />
-            <div>
-              <h4>Summary</h4>
               <div
                 class="field__item"
                 dangerouslySetInnerHTML={{
@@ -71,41 +128,89 @@ const MeasureView = (props) => {
             <br />
             <div class="fullwidthZ">
               <div class="images-container">
-                <h4>Illustration(s)</h4>
                 <div class="image-flexbox">
-                  {content.items.map(
-                    (item) =>
-                      item['@type'] === 'Image' && (
-                        <div class="image-wrapper">
-                          <div>
-                            <a href={item['@id'] + '/@@images/image'}>
-                              <LazyLoadImage
-                                src={item['@id'] + '/@@images/image/preview'}
-                                title={item.title}
-                                alt={item.title}
-                              />
-                            </a>
-                          </div>
-                          <div>{item.title}</div>
-                          {item.description.includes('http') ? (
+                  <div>
+                    {content.items.map(
+                      (item) =>
+                        item['@type'] === 'Image' && (
+                          <div class="image-wrapper">
                             <div>
-                              <a href={item.description.split(': ')[1]}>
-                                Source
+                              <a href={item['@id'] + '/@@images/image'}>
+                                <LazyLoadImage
+                                  src={item['@id'] + '/@@images/image/preview'}
+                                  title={item.title}
+                                  alt={item.title}
+                                />
                               </a>
                             </div>
-                          ) : (
-                            <div>Source: {item.description.split(': ')[1]}</div>
+                            <div>
+                              <div class="image-title">{item.title}</div>
+                              {item.description.includes('http') ? (
+                                <div>
+                                  <a href={item.description.split(': ')[1]}>
+                                    Source
+                                  </a>
+                                </div>
+                              ) : (
+                                <div class="image-source">
+                                  Source: {item.description.split(': ')[1]}
+                                </div>
+                              )}
+                            </div>
+                          </div>
+                        ),
+                    )}
+                  </div>
+                  <div>
+                    <div class="field--label-inline">
+                      <div class="field__label">NWRM code</div>
+                      <div class="field__item">{content.measure_code}</div>
+                    </div>
+
+                    <div class="field--label-inline">
+                      <div class="field__label">Sector</div>
+                      <div class="field__item">{content.measure_sector}</div>
+                    </div>
+
+                    {content.other_sector && (
+                      <>
+                        <br />
+                        <div class="field--label-inline">
+                          <div class="field__label">Other sector(s)</div>
+                          <div class="field__item">{content.other_sector}</div>
+                        </div>
+                      </>
+                    )}
+
+                    <div>
+                      <div class="field--label-inline">
+                        <div class="field__label">Complete description</div>
+                        <div class="field__item">
+                          {content.items.map(
+                            (item) =>
+                              item['@type'] === 'File' && (
+                                <a
+                                  href={item['@id'] + '/@@images/file'}
+                                  rel="noreferrer"
+                                  target="_blank"
+                                >
+                                  {item.title}
+                                </a>
+                              ),
                           )}
                         </div>
-                      ),
-                  )}
+                      </div>
+                    </div>
+
+                    <br />
+                  </div>
                 </div>
               </div>
             </div>
 
             <br />
             <div>
-              <h4>Possible benefits with level</h4>
+              <h3>Benefits</h3>
               <div
                 class="field__item"
                 dangerouslySetInnerHTML={{
@@ -117,7 +222,7 @@ const MeasureView = (props) => {
             <br />
             {content.case_studies && (
               <div>
-                <h4>Case studies</h4>
+                <h3>Case studies</h3>
                 <div class="field__item">
                   <ul>
                     {content.case_studies.map((item) => (
